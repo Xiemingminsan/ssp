fb8df93388def6fcbb6fe20238fe65b951808a63
Xiemingminsan <minshahat@gmail.com>
2024-09-03 23:49:55 +0300

    mobile apps banned

--- app/notallowed/page.jsx
+++ app/notallowed/page.jsx
@@ -1,9 +1,23 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 // app/not-allowed/page.js
 
 export default function NotAllowed() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const isDesktop = typeof window !== "undefined" && window.innerWidth > 1024;
+
+    if (isDesktop) {
+      router.push("/"); // Redirect to the homepage or another page
+    }
+  }, [router]);
+
   return (
     <div>
-      <h1>Nada! Access Denied</h1>
+      <h1>Access Denied</h1>
       <p>This website is only accessible from a desktop device.</p>
       <p
         style={{
